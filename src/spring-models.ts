f6acb42805e3c5638f7f21bbaf363b5915fdb95c
Gido Manders <gido.manders@42.nl>
2023-02-24 12:50:55 +0100

    feature: pageOf utility
    Sometimes it would be useful to paginate an array of items where a
    component only accepts a page while only a full list is provided.
    
    Added pageOf utility function.

--- src/spring-models.ts
+++ src/spring-models.ts
@@ -29,3 +29,32 @@ export function emptyPage<T>(): Page<T> {
     numberOfElements: 0
   });
 }
+
+/**
+ * Create a Page of a subset of an array of items.
+ */
+export function pageOf<T>(
+  content: T[],
+  page = 1,
+  size = 10,
+  oneBased = true
+): Page<T> {
+  const actualPage = oneBased ? page - 1 : page;
+
+  const offset = actualPage * size;
+
+  const slice = content.slice(offset, offset + size);
+
+  const totalPages = Math.max(1, Math.ceil(content.length / size));
+
+  return Object.freeze({
+    content: slice,
+    last: oneBased ? page === totalPages : page === totalPages - 1,
+    totalElements: content.length,
+    totalPages,
+    size: slice.length,
+    number: page,
+    first: oneBased ? page === 1 : page === 0,
+    numberOfElements: slice.length
+  });
+}
